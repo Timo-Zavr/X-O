30e5d9f085c4f5618c73ba0e34cf6185ec244dea
TimoZavr <zaraisky.timaeus@gmail.com>
2024-07-01 17:30:02 +0300

    !!!

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ let O = true
 in1.addEventListener("click", (event) => {
     if (O === true){
         in1.textContent = 'O'
+        in1.style.color = "blue"
         X = true
         O = false
     } else if (X === true){
         in1.textContent = 'X'
+        in1.style.color = "red"
         X = false
         O = true
     }
@@ -25,10 +27,12 @@ in1.addEventListener("click", (event) => {
 in2.addEventListener("click", (event) => {
     if (O === true){
         in2.textContent = 'O'
+        in2.style.color = "blue"
         X = true
         O = false
     } else if (X === true){
         in2.textContent = 'X'
+        in2.style.color = "red"
         X = false
         O = true
     }
@@ -36,10 +40,12 @@ in2.addEventListener("click", (event) => {
 in3.addEventListener("click", (event) => {
     if (O === true){
         in3.textContent = 'O'
+        in3.style.color = "blue"
         X = true
         O = false
     } else if (X === true){
         in3.textContent = 'X'
+        in3.style.color = "red"
         X = false
         O = true
     }
@@ -47,10 +53,12 @@ in3.addEventListener("click", (event) => {
 in4.addEventListener("click", (event) => {
     if (O === true){
         in4.textContent = 'O'
+        in4.style.color = "blue"
         X = true
         O = false
     } else if (X === true){
         in4.textContent = 'X'
+        in4.style.color = "red"
         X = false
         O = true
     }
@@ -58,10 +66,12 @@ in4.addEventListener("click", (event) => {
 in5.addEventListener("click", (event) => {
     if (O === true){
         in5.textContent = 'O'
+        in5.style.color = "blue"
         X = true
         O = false
     } else if (X === true){
         in5.textContent = 'X'
+        in5.style.color = "red"
         X = false
         O = true
     }
@@ -69,10 +79,12 @@ in5.addEventListener("click", (event) => {
 in6.addEventListener("click", (event) => {
     if (O === true){
         in6.textContent = 'O'
+        in6.style.color = "blue"
         X = true
         O = false
     } else if (X === true){
         in6.textContent = 'X'
+        in6.style.color = "red"
         X = false
         O = true
     }
@@ -80,10 +92,12 @@ in6.addEventListener("click", (event) => {
 in7.addEventListener("click", (event) => {
     if (O === true){
         in7.textContent = 'O'
+        in7.style.color = "blue"
         X = true
         O = false
     } else if (X === true){
         in7.textContent = 'X'
+        in7.style.color = "red"
         X = false
         O = true
     }
@@ -91,10 +105,12 @@ in7.addEventListener("click", (event) => {
 in8.addEventListener("click", (event) => {
     if (O === true){
         in8.textContent = 'O'
+        in8.style.color = "blue"
         X = true
         O = false
     } else if (X === true){
         in8.textContent = 'X'
+        in8.style.color = "red"
         X = false
         O = true
     }
@@ -102,11 +118,19 @@ in8.addEventListener("click", (event) => {
 in9.addEventListener("click", (event) => {
     if (O === true){
         in9.textContent = 'O'
+        in9.style.color = "blue"
         X = true
         O = false
     } else if (X === true){
         in9.textContent = 'X'
+        in9.style.color = "red"
         X = false
         O = true
     }
 })
+
+if(in1.textContent === "O" && in2.textContent === "O" && in3.textContent === "O"){
+    X = false
+    O = false
+    console.log("O win")
+}
