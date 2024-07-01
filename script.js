9ad50d483d68181b4e4d9987c418457bbb275ff8
TimoZavr <zaraisky.timaeus@gmail.com>
2024-07-01 15:09:15 +0300

    x-o

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const in1 = document.querySelector("#1")
-const in2 = document.querySelector("#2")
-const in3 = document.querySelector("#3")
-const in4 = document.querySelector("#4")
-const in5 = document.querySelector("#5")
-const in6 = document.querySelector("#6")
-const in7 = document.querySelector("#7")
-const in8 = document.querySelector("#8")
-const in9 = document.querySelector("#9")
+const in1 = document.querySelector("#a")
+const in2 = document.querySelector("#b")
+const in3 = document.querySelector("#c")
+const in4 = document.querySelector("#d")
+const in5 = document.querySelector("#e")
+const in6 = document.querySelector("#f")
+const in7 = document.querySelector("#g")
+const in8 = document.querySelector("#h")
+const in9 = document.querySelector("#i")
 
 let X = false
 let O = true
@@ -29,6 +29,83 @@ in2.addEventListener("click", (event) => {
         O = false
     } else if (X === true){
         in2.textContent = 'X'
+        X = false
+        O = true
+    }
+})
+in3.addEventListener("click", (event) => {
+    if (O === true){
+        in3.textContent = 'O'
+        X = true
+        O = false
+    } else if (X === true){
+        in3.textContent = 'X'
+        X = false
+        O = true
+    }
+})
+in4.addEventListener("click", (event) => {
+    if (O === true){
+        in4.textContent = 'O'
+        X = true
+        O = false
+    } else if (X === true){
+        in4.textContent = 'X'
+        X = false
+        O = true
+    }
+})
+in5.addEventListener("click", (event) => {
+    if (O === true){
+        in5.textContent = 'O'
+        X = true
+        O = false
+    } else if (X === true){
+        in5.textContent = 'X'
+        X = false
+        O = true
+    }
+})
+in6.addEventListener("click", (event) => {
+    if (O === true){
+        in6.textContent = 'O'
+        X = true
+        O = false
+    } else if (X === true){
+        in6.textContent = 'X'
+        X = false
+        O = true
+    }
+})
+in7.addEventListener("click", (event) => {
+    if (O === true){
+        in7.textContent = 'O'
+        X = true
+        O = false
+    } else if (X === true){
+        in7.textContent = 'X'
+        X = false
+        O = true
+    }
+})
+in8.addEventListener("click", (event) => {
+    if (O === true){
+        in8.textContent = 'O'
+        X = true
+        O = false
+    } else if (X === true){
+        in8.textContent = 'X'
+        X = false
+        O = true
+    }
+})
+in9.addEventListener("click", (event) => {
+    if (O === true){
+        in9.textContent = 'O'
+        X = true
+        O = false
+    } else if (X === true){
+        in9.textContent = 'X'
         X = false
         O = true
     }
